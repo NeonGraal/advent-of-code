e3dfd7e925e523331346cbe9fc4db94c20562fac
NeonGraal <github@scifi.geek.nz>
2022-01-23 08:11:47 +1300

    AoC C# Templates

---
+++ AoCTemplate/AoC-Day.cs
@@ -0,0 +1,20 @@
+﻿namespace $rootnamespace$;
+
+public class $safeitemname$ : DayOfAdvent<$safeitemname$>, IDayOfAdvent
+{
+  public int Part1()
+  {
+    throw new NotImplementedException();
+  }
+  public string Part1Result() =>
+    $"{Part1()}";
+
+  public int Part2()
+  {
+    throw new NotImplementedException();
+  }
+
+  public string Part2Result() =>
+    $"{Part2()}";
+  }
+}
---
+++ AoCTemplate/AoC-DayTests.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+
+namespace $rootnamespace$;
+
+public class $safeitemname$Tests
+{
+  readonly $safeitemname$ day = new();
+
+  [Theory]
+  [InlineData("", 0)]
+  public void Part1(string input, int expected)
+  {
+    day.SetInput(input);
+
+    var result = day.Part1();
+
+    Assert.Equal(expected, result);
+  }
+
+  [Theory]
+  [InlineData("", 1)]
+  public void Part2(string input, int expected)
+  {
+    day.SetInput(input);
+
+    var result = day.Part2();
+
+    Assert.Equal(expected, result);
+  }
+}

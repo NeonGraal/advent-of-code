ddc1e9c9e53bcf0afa208626aa930d4206d59872
NeonGraal <github@scifi.geek.nz>
2022-02-08 22:49:44 +1300

    AoC 2016 Day 5

---
+++ Advent2016/src/day_01_08/day05.rs
@@ -0,0 +1,90 @@
+use crate::shared::input_string;
+
+use md5;
+
+const DAY_NAME: &str = "day05";
+
+pub fn run(suffix: &str) {
+    let s = input_string(DAY_NAME, suffix);
+
+    let result = part1(&s);
+    println!("Part1: {}", result);
+
+    let result = part2(&s);
+    println!("Part2: {}", result);
+}
+
+fn hashes(salt: &str) -> impl Iterator<Item = (u8, u8)> + '_ {
+    let mut i = 0;
+
+    let collector = std::iter::from_fn(move || {
+        let mut result: [u8; 16] = [1; 16];
+
+        while result[0] != 0 || result[1] != 0 || result[2] > 15 {
+            let data = format!("{}{}", salt, i);
+            let digest = md5::compute(data.to_string());
+            result = <[u8; 16]>::from(digest);
+            i = i + 1;
+            if i % 20000 == 0 {
+                print!(".");
+                if i % 1000000 == 0 {
+                    println!(" {}", i);
+                }
+            }
+        }
+        print!("!");
+
+        Some((result[2], result[3]))
+    });
+
+    collector
+}
+
+fn part1(lines: &String) -> String {
+    let s: String = hashes(lines)
+        .take(8)
+        .map(|r| format!("{:x}", r.0))
+        .collect();
+
+    println!("");
+
+    return s;
+}
+
+fn part2(lines: &String) -> String {
+    let mut s = ['_'; 8];
+
+    for (pos, digit) in hashes(lines) {
+        let i = pos as usize;
+        if i < 8 && s[i] == '_' {
+            s[i] = format!("{:02x}", digit).chars().next().unwrap();
+        }
+        if s.iter().all(|&c| c != '_') {
+            return s.iter().collect();
+        }
+    }
+    "".to_string()
+}
+
+mod tests {
+    #[allow(unused_imports)]
+    use super::{input_string, part1, part2, DAY_NAME};
+
+    #[test]
+    fn first() {
+        let lines = input_string(DAY_NAME, ".sample");
+
+        let result = part1(&lines);
+
+        assert_eq!(result, "18f47a30");
+    }
+
+    #[test]
+    fn second() {
+        let lines = input_string(DAY_NAME, ".sample");
+
+        let result = part2(&lines);
+
+        assert_eq!(result, "05ace8e3");
+    }
+}

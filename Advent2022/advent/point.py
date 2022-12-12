b8f712d73382edc624fbf6142973f0733b10e306
Struan Judd <github@scifi.geek.nz>
2022-12-12 22:29:38 +1300

    AoC 2022 Move Point to common

---
+++ Advent2022/advent/point.py
@@ -0,0 +1,28 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __hash__(self):
+        return hash(self.x + 999) * 3333 + hash(self.y + 999)
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __repr__(self):
+        return f"{self.x},{self.y}"
+
+    def __iadd__(self, other):
+        self.x += other.x
+        self.y += other.y
+        return self
+
+    def __irshift__(self, other):
+        (dx, dy) = (other.x - self.x, other.y - self.y)
+        (ax, ay) = (abs(dx), abs(dy))
+        if ax > 1 or ay > 1:
+            if ax > 0:
+                self.x += dx // ax
+            if ay > 0:
+                self.y += dy // ay
+        return self

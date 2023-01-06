d3380be4c1863214a2e4c50c0027fb4bf5d59250
Struan Judd <neongraal@github.com>
2023-01-06 18:13:58 +1300

    AoC 2022 Day 25

--- Advent2022/src/day_25.py
+++ Advent2022/src/day_25.py
@@ -7,8 +7,62 @@ from advent import Advent
 #fmt: on
 
 
+def fromSnafu(input: str) -> int:
+    num = 0
+    for c in input:
+        num *= 5
+        match c:
+            case '2': num += 2
+            case '1': num += 1
+            case '0': num += 0
+            case '-': num += -1
+            case '=': num += -2
+            case _: raise Exception(f"Invalid SNAFU digit {c}")
+    return num
+
+
+def toSnafu(input: int) -> str:
+
+    result = ""
+    while True:
+        r = input % 5
+        inc = 0
+        match r:
+            case 2: result = "2" + result
+            case 1: result = "1" + result
+            case 0: result = "0" + result
+            case 4:
+                result = "-" + result
+                inc = 1
+            case 3:
+                result = "=" + result
+                inc = 1
+            case _: raise Exception("Invalid SNAFU remainder {r}")
+        if abs(input) < 3:
+            return result
+        input //= 5
+        input += inc
+
+
 class Day(Advent):
     day = "25"
+
+    def parseState(self, f):
+        self.sum = 0
+
+    def parseCommand(self, line: str):
+        num = fromSnafu(line)
+        # print(f"{line} -> {num} ({toSnafu(num)})")
+        return num
+
+    def process1(self, command):
+        self.sum += command
+
+    def result1(self):
+        return toSnafu(self.sum)
+
+    def result2(self):
+        pass
 
 
 day = Day()
